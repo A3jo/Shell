0d9c00d92f80fd9ee5f231be4b48cc80ee3deddc
A3jo <146429953+A3jo@users.noreply.github.com>
2024-03-19 15:16:45 +0530

    prgm5.sh

--- networking/prgm5.sh
+++ networking/prgm5.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 #Author : Ajo Jojo
 echo "Author:Ajo jojo"
-echo Enter the string 
+echo "Enter the string:"
 read s
-echo $s>temp
-rvs="$(rev temp)"
-if [ $s=$rvs ]
-then
-echo "$s is palindrome"
+rvs=$(echo $s | rev)
+if [ $s = $rvs ]; then
+    echo "$s is a palindrome"
 else
-echo "$s is not a plaindrome"
+    echo "$s is not a palindrome"
 fi
